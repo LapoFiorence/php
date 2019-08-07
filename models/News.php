da1589c2a58c3adab652df6c1ebbd1971da51724
V.Dolgova <lapo_fiorence@mail.ru>
2019-08-07 17:29:52 +0300

    +

--- models/News.php
+++ models/News.php
@@ -1,8 +1,13 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class News
+{
+    public static function getNewsItemById($id)
+    {
+        //Запрос к БД
+    }
+    
+    public static function getNewsList()
+    {
+        //Запрос к БД
+    }
+}
