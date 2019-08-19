04684a9e1817eaeabe00fa8c2c4deb691f7aa63c
V.Dolgova <lapo_fiorence@mail.ru>
2019-08-19 11:56:26 +0300

    обновление

--- config/routes.php
+++ config/routes.php
@@ -1,18 +1,5 @@
 <?php
 return array(
-    
-
-    'news/([a-z]+)/([0-9]+)' => 'news/view/$1,$2',
-    
-//    'news' => 'news/index',
-    'news' => 'view/news',// при запросе news будет вызван метод actionView в NewsController
-
-    'news/([a-z]+)/([0-9]+)' => 'news/view/$1/$2',
-    
-//    'news' => 'news/index',
-    'products' => 'product/list',// при запросе news будет вызван метод actionView в NewsController
-    'news' => 'news/index',
-
-//    'products' => 'product/list',
+    '' => 'site/index', // actionIndex в SiteController
 );
 
