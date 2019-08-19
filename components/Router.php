27d76f443090ef9110de21e21b1fe85329a91f40
Ad-Ok <adolf_oknov@mail.ru>
2019-08-19 22:56:25 +0300

    remove DIE command

--- components/Router.php
+++ components/Router.php
@@ -7,77 +7,64 @@ class Router {
         $routesPath= ROOT.'/config/routes.php'; // указываем путь к роутам
 
         $this->routes = include($routesPath); // присваиваем свойству routes массив
-        echo $routesPath;
 
         $this->routes = include($routesPath); // присваиваем свойству routes массив, который хранится в файле routes.php
 
     }
-    
+
     private function getURI()//метод возвращает строку
     {
        if (!empty($_SERVER['REQUEST_URI'])){
-            return trim($_SERVER['REQUEST_URI'], '/'); 
+            return trim($_SERVER['REQUEST_URI'], '/');
     }
     }
 
     public function run()// отвечает за анализ запроса и передачу управления
-    {     
+    {
 //         Получить строку запроса
         $uri = $this->getURI();
-        echo $uri;
 //        print_r($this->routes);//проверка
-        
+
         // Проверить наличие такого запроса в routes.php
         foreach ($this->routes as $uriPattern => $path){// для каждого маршрута, находящегося в массиве помещаем в переменную $uriPattern строку запроса из routes.php, а в переменную $path мы помещаем путь 'news/index'
-           echo "<br>$uriPattern -> $path"; //обязательны двойные кавычки
-//            
+//
             // Сравниваем $uriPattern и $uri
             if (preg_match("~$uriPattern~", $uri)){
-                
+
 //                echo '<br>Где ищем (запрос, который набрал пользователь): '.$uri;
 //                echo '<br>Что ищем (совпадение из правила): '.$uriPattern;
 //                echo '<br>Кто обрабатывает: '.$path;
-//                
+//
                 //Получаем внутренний путь из внешнего согласно правилу.
                 $internalRoute = preg_replace("~$uriPattern~", $path, $uri); // в строке запроса мы ищем параметры $path и $uri по определенному шаблону $uriPattern
-                
+
 //                echo '<br><br>Нужно сформировать: '.$internalRoute;
-                
-                
+
+
                 // Определить какой контроллер и action обрабатывают запрос
                 $segments = explode('/', $internalRoute); // explode делит строку на две части
 //                echo '<pre>';
 //                print_r($segments);
 //                echo '</pre>';
-                
+
                 $controllerName = array_shift($segments).'Controller'; // получение имени контроллера, функция array_shift получает значение первого элемента в массиве и удаляет его из массива
 //                echo $controllerName;
                 $controllerName = ucfirst($controllerName);
-                echo $controllerName;
-                
+
                 $actionName = 'action'.ucfirst(array_shift($segments));
-                
-                                
-                echo '<br>controller name: '.$controllerName;
-                echo '<br>action name: '.$actionName;
+
                 $parameters = $segments;
-                echo '<pre>';
-                print_r($parameters);
-                
-                die;
-                
+
+
                 //Подключить файл класса-контроллера
 
-                $controllerFile = ROOT . '/controller/' .// перенос
-                $controllerName . '.php';
+                $controllerFile = ROOT . '/controllers/' . $controllerName . '.php';
 
-                $controllerFile = ROOT . '/controllers/' . $controllerName . '.php'; 
 
-                
                 if (file_exists($controllerFile)){
                     include_once ($controllerFile);
                 }
-                
+
 
                 // Создать объект, вызвать метод (т.е. action)
                 $controllerObject = new $controllerName;
@@ -92,21 +79,21 @@ class Router {
                 }
             }
         }
-        
+
     }
-        
-        
-        
+
+
+
         // если есть совпадение, определить какой контроллер
         // и action обрабатывабт запрос
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
+
+
+
+
+
+
+
+
+
+
 }
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,10 +2,10 @@
 
 class SiteController
 {
-    public function actionIndex () 
+    public function actionIndex ()
     {
         require_once(ROOT . '/views/site/index.php');
         
-        return true;        
+        return true;
     }
 }
