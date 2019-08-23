8b45bb8a470b5fdcfd76bd141397fe5d7ed36aa0
Ad-Ok <adolf_oknov@mail.ru>
2019-08-24 02:33:17 +0300

    make the category works

--- models/Category.php
+++ models/Category.php
@@ -4,19 +4,18 @@ class Category
 {
     public static function getCategoriesList()//возвращает список категорий
     {
-//        $db = Db::getConnection();
-//        
-//        $result = $db->query('SELECT "id", "name"  FROM "category" WHERE status = "1" ORDER BY "sort_order", name ASC');
-//        
-//        $categoryList = array();
-//                        
-//        $i = 0;
-//        while ($row = $result->fetch()) {
-//            $categoryList[$i]['id'] = $row['id'];
-//            $categoryList[$i]['name'] = $row['name'];
-//            $i++;
-//        }
-        
-//        return $categoryList;
+       $db = Db::getConnection();
+
+       $result = $db->query('SELECT id, name FROM category WHERE status = 1 ORDER BY sort_order, name ASC');
+
+       $categoryList = array();
+
+       $i = 0;
+       while ($row = $result->fetch()) {
+           $categoryList[$i]['id'] = $row['id'];
+           $categoryList[$i]['name'] = $row['name'];
+           $i++;
+       }
+       return $categoryList;
     }
 }
