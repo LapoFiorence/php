7484f57986af47c12bd3ff109ce70e245f808fac
V.Dolgova <lapo_fiorence@mail.ru>
2019-08-26 09:20:24 +0300

    no message

--- models/Category.php
+++ models/Category.php
@@ -4,19 +4,33 @@ class Category
 {
     public static function getCategoriesList()//возвращает список категорий
     {
-//        $db = Db::getConnection();
+        $db = Db::getConnection();
+        
+        $result = $db->query('SELECT `id`, `name`  FROM `category` WHERE status = `1` ORDER BY `sort_order`');
+        
+        $categoryList = array();
+                        
+        $i = 0;
+        while ($row = $result->fetch()) {
+            $categoryList[$i]['id'] = $row['id'];
+            $categoryList[$i]['name'] = $row['name'];
+            $i++;
+        }
 //        
-//        $result = $db->query('SELECT "id", "name"  FROM "category" WHERE status = "1" ORDER BY "sort_order", name ASC');
+        return $categoryList;      
+        
+//        $con = mysqli_connect("localhost", "root", "root", "super_mag");
+//        if (mysqli_connect_errno()){
+//            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//        }
 //        
-//        $categoryList = array();
-//                        
+//        $result = mysqli_query($con, 'SELECT `id`, `name` * FROM `category` WHERE status = `1` ORDER BY `sort_order`, name ASC');
+//        
 //        $i = 0;
-//        while ($row = $result->fetch()) {
+//        while ($row = mysqli_fetch_array($result)){
 //            $categoryList[$i]['id'] = $row['id'];
 //            $categoryList[$i]['name'] = $row['name'];
 //            $i++;
 //        }
-        
-//        return $categoryList;
     }
 }
