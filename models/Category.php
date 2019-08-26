e1ff199f6898cf028a62dbf9c39470794967449b
V.Dolgova <lapo_fiorence@mail.ru>
2019-08-26 09:21:39 +0300

    no message

--- models/Category.php
+++ models/Category.php
@@ -4,6 +4,7 @@ class Category
 {
     public static function getCategoriesList()//возвращает список категорий
     {
+<<<<<<< HEAD
         $db = Db::getConnection();
         
         $result = $db->query('SELECT `id`, `name`  FROM `category` WHERE status = `1` ORDER BY `sort_order`');
@@ -32,5 +33,20 @@ class Category
 //            $categoryList[$i]['name'] = $row['name'];
 //            $i++;
 //        }
+=======
+       $db = Db::getConnection();
+
+       $result = $db->query('SELECT id, name FROM category WHERE status = 1 ORDER BY sort_order, name ASC');
+
+       $categoryList = array();
+
+       $i = 0;
+       while ($row = $result->fetch()) {
+           $categoryList[$i]['id'] = $row['id'];
+           $categoryList[$i]['name'] = $row['name'];
+           $i++;
+       }
+       return $categoryList;
+>>>>>>> e42fb5bee29db46c9fc8d16cd8526d0527de7908
     }
 }
