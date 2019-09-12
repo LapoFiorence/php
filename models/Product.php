5463527e4e20f55c3d9df17f17b8c2104005adce
V.Dolgova <lapo_fiorence@mail.ru>
2019-09-12 17:46:53 +0300

    no message

--- models/Product.php
+++ models/Product.php
@@ -27,4 +27,27 @@ class Product
         
         return $productsList;
     }
+    
+    public static function getProductsListByCategory($categoryId = false)
+    {
+        if ($categoryId) {
+            $db = Db::getConnection();
+            $products = array();
+            $result = $db->query("SELECT id, name, price, is_new FROM product WHERE status = 1 ORDER BY id DESC");
+//                    . "WHERE status = 1 AND category_id = '$categoryId'"
+//                    . "ORDER BY id DESC");
+//                    . "LIMIT".self::SHOW_BY_DEFAULT);
+            
+            $i = 0;
+            while ($row = $result->fetch()) {
+                $products[$i]['id'] = $row['id'];
+                $products[$i]['name'] = $row['id'];
+                $products[$i]['price'] = $row['id'];
+                $products[$i]['is_new'] = $row['id'];
+                $i++;
+            }
+            
+            return $products;
+        }
+    }
 }
