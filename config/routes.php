465a9e9f9d1b1517bf03a50a1703873444dcd9b3
lapo_fiorence <lapo_fiorence@mail.ru>
2019-11-28 23:07:56 +0300

    add route for user/register

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,7 @@ return array(
     'product/([0-9]+)'=>'product/view/$1', // actionView в ProductController
 
     'catalog'=>'catalog/index', // actionIndex в CatalogController
+    'user/register'=>'user/register', // user registation page
     
     'category/([0-9]+)/page-([0-9]+)' => 'catalog/category/$1/$2',
     'category/([0-9]+)' => 'catalog/category/$1', // actionCategory в CatalogController
