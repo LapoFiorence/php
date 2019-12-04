bdcfcbb74b13c25471a2fac4157df8879fc0f9a9
V.Dolgova <lapo_fiorence@mail.ru>
2019-12-04 18:10:22 +0300

    no message

--- models/User.php
+++ models/User.php
@@ -53,4 +53,63 @@ class User
             return true;  
         return false;
     }
+    
+    public static function checkUserData($email, $password)
+    {
+        $db = Db::getConnection();
+        
+        $sql = 'SELECT * FROM user WHERE email = :email AND password = :password';
+        
+        $result = $db->prepare($sql);
+        $result->bindParam(':email', $email, PDO::PARAM_INT);
+        $result->bindParam(':password', $password, PDO::PARAM_INT);
+        $result->execute();
+        
+        $user = $result->fetch();
+        if($user) {
+            return $user['id'];
+        }
+        return false;
+    }
+    
+    // Запоминаем пользователя
+    public static function auth($userId)
+    {        
+        $_SESSION['user'] = $userId;
+    }
+    
+    public static function checkLogged()
+    {        
+        //если сессия есть, вернем идентификатор пользователя
+        if (isset($_SESSION['user'])){
+            return $_SESSION['user'];
+        }
+        
+        header("Location: /user/login");
+    }
+    
+    public static function isGuest()
+    {        
+        if (isset($_SESSION['user'])) {
+            return false;
+        }
+        return true;
+    }
+    
+    public static function getUserById($id)
+    {
+        if ($id) {
+            $db = Db::getConnection();
+            $sql = 'SELECT * FROM user WHERE id = :id';
+            
+            $result = $db->prepare($sql);
+            $result->bindParam(':id', $id, PDO::PARAM_INT);
+            
+            //Указываем, что хотим получить данные в виде массива
+            $result->setFetchMode(PDO::FETCH_ASSOC);
+            $result->execute();
+            
+            return $result->fetch();
+        }
+    }
 }
